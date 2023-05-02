8b690b06c51a46bdd8b25ce442f4e43baa51c683
c4chris124 <c4chris124@gmail.com>
2023-05-02 11:51:00 -0600

    install jwt package to root of proyect, modified models user to return uid insted of mongoose id, created new helper for generate jwt

---
+++ helpers/generate-jwt.js
@@ -0,0 +1,24 @@
+import jwt from "jsonwebtoken"
+
+const generateJWT = (uid = "") => {
+  return new Promise((resolve, reject) => {
+    const payload = { uid }
+    jwt.sign(
+      payload,
+      process.env.SECRETORPRIVATEKEY,
+      {
+        expiresIn: "4h"
+      },
+      (err, token) => {
+        if (err) {
+          console.log(err)
+          reject("Not able to generate token")
+        } else {
+          resolve(token)
+        }
+      }
+    )
+  })
+}
+
+export { generateJWT }
