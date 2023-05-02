3135021cde6db8d20d501708f0a21a4312fc606b
c4chris124 <c4chris124@gmail.com>
2023-05-01 19:30:20 -0600

    created controller for login, email, user, anb pass validation

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,9 +1,40 @@
 import { response, request } from "express"
+import User from "../models/user.js"
+import bcryptjs from "bcryptjs"
 
-const login = (req = request, res = response) => {
-  res.json({
-    msg: "Login Ok"
-  })
+const login = async (req = request, res = response) => {
+  const { email, password } = req.body
+  try {
+    // verify if email exist
+    const user = await User.findOne({ email })
+    if (!user) {
+      return res.status(400).json({
+        msg: "User / Password are not correct - email"
+      })
+    }
+    // if user is active
+    if (!user.status) {
+      return res.status(400).json({
+        msg: "User / Password are not correct - status: false"
+      })
+    }
+    // verify password
+    const validPassword = bcryptjs.compareSync(password, user.password)
+    if (!validPassword) {
+      return res.status(400).json({
+        msg: "User / Password are not correct - password"
+      })
+    }
+    // generate JWT
+    res.json({
+      msg: "Login Ok"
+    })
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({
+      msg: "Talk to the Admin"
+    })
+  }
 }
 
 export { login }
