77585212321f605b6bffa1b159fcbc89b88370e0
c4chris124 <c4chris124@gmail.com>
2023-05-01 19:29:28 -0600

    Added post login router and middlewares for email and pass

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,9 +1,18 @@
 import { Router } from "express"
 import { check } from "express-validator"
 import { login } from "../controllers/auth.controller.js"
+import { validateFields } from "../middlewares/validate-fields.js"
 
 const router = Router()
 
-router.post("/login", login)
+router.post(
+  "/login",
+  [
+    check("email", "The Email is a must").isEmail(),
+    check("password", "The password is a must").not().isEmpty(),
+    validateFields
+  ],
+  login
+)
 
 export default router
