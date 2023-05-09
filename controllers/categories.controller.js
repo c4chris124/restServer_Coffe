39c3e4450035cd1ff2881c982287da5059bb383c
c4chris124 <c4chris124@gmail.com>
2023-05-08 22:20:49 -0600

    Creating basci controllers, added to routes

--- controllers/categories.controller.js
+++ controllers/categories.controller.js
@@ -1,6 +1,29 @@
 import { response, request } from "express"
 import { Category } from "../models/index.js"
 
+// obtain categories - paginated - total - populate
+const getCategories = async (req = request, res = response) => {
+  const { limit = 5, since = 0 } = req.query
+  const query = { status: true }
+
+  const [total, categories] = await Promise.all([
+    Category.countDocuments(query),
+    Category.find(query)
+      .skip(Number(since))
+      .limit(Number(limit))
+      .populate("user")
+  ])
+  res.json({ total, categories })
+}
+
+// obtain category - populate - {}
+const getCategoryById = async (req = request, res = response) => {
+  const { id } = req.params
+  const category = await Category.findById(id)
+  res.json(category)
+}
+
+// create category
 const createCategory = async (req = request, res = response) => {
   const name = req.body.name.toUpperCase()
   const categoryDb = await Category.findOne({ name })
@@ -24,4 +47,24 @@ const createCategory = async (req = request, res = response) => {
   res.status(201).json(category)
 }
 
-export { createCategory }
+// update category
+const updateCategory = async (req = request, res = response) => {
+  const { id } = req.params
+  const { name } = req.body
+  const categoryDB = await Category.findByIdAndUpdate(id, name)
+  res.json(categoryDB)
+}
+// delete category - status:false
+const deleteCategory = async (req = request, res = response) => {
+  const { id } = req.params
+  const category = await Category.findOneAndUpdate(id, { status: false })
+  res.json(category)
+}
+
+export {
+  getCategories,
+  getCategoryById,
+  createCategory,
+  updateCategory,
+  deleteCategory
+}
--- routes/categories.routes.js
+++ routes/categories.routes.js
@@ -1,21 +1,28 @@
 import { Router } from "express"
 import { check } from "express-validator"
-import { validateFields, validateJWT } from "../middlewares/index.js"
-import { createCategory } from "../controllers/categories.controller.js"
+import {
+  isAdminRole,
+  validateFields,
+  validateJWT
+} from "../middlewares/index.js"
+import {
+  createCategory,
+  deleteCategory,
+  getCategories,
+  getCategoryById,
+  updateCategory
+} from "../controllers/categories.controller.js"
 
 const router = Router()
 
 /* {{url/aoi/categories}} */
 
 // obtain all categories - public
-router.get("/", (req, res) => {
-  res.json("get")
-})
+router.get("/", getCategories)
 
 // obtain a category by id - public
-router.get("/:id", (req, res) => {
-  res.json("get - id")
-})
+// [check("id").custom(categoryExist)]
+router.get("/:id", getCategoryById)
 // create category - private - anyone with a valid token
 router.post(
   "/",
@@ -28,13 +35,13 @@ router.post(
 )
 
 // update - private - anyone with valid token
-router.put("/:id", (req, res) => {
-  res.json("put")
-})
+router.put("/:id", updateCategory)
 
 // delete - Admin
-router.delete("/:id", (req, res) => {
-  res.json("delete")
-})
+router.delete(
+  "/:id",
+  [validateJWT, isAdminRole, validateFields],
+  deleteCategory
+)
 
 export default router
