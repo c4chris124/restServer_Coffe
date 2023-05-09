c325bf80288db2064f8aa2d3e36f8e522b40b421
c4chris124 <c4chris124@gmail.com>
2023-05-09 15:47:56 -0600

    Created route for post a new product

---
+++ routes/products.routes.js
@@ -0,0 +1,29 @@
+import { Router } from "express"
+import { check } from "express-validator"
+import { hasRole, validateFields, validateJWT } from "../middlewares/index.js"
+import { createProduct } from "../controllers/products.controller.js"
+import { categoryExistById } from "../helpers/db-validators.js"
+const router = Router()
+
+// router.get('/')
+
+// router.get('/')
+
+// create category - private - anyone with a valid token
+router.post(
+  "/",
+  [
+    validateJWT,
+    check("name", "name is a must").not().isEmpty(),
+    check("category").custom(categoryExistById),
+    validateFields
+  ],
+  createProduct
+)
+
+// router.put('/')
+
+// router.delete('/')
+
+export default router
+// 64598e7d9768c07f9092a5df
