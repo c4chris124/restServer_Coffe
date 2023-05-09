54723a711fd6aa47c162d1a72456bdd772ca264b
c4chris124 <c4chris124@gmail.com>
2023-05-09 15:31:57 -0600

    Creating product model

---
+++ models/product.js
@@ -0,0 +1,42 @@
+import { Schema, model } from "mongoose"
+
+const ProductSchema = Schema({
+  name: {
+    type: String,
+    required: [true, "Name is a must"],
+    unique: true
+  },
+  status: {
+    type: Boolean,
+    default: true,
+    require: true
+  },
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    require: true
+  },
+  price: {
+    type: Number,
+    default: 0
+  },
+  category: {
+    type: Schema.Types.ObjectId,
+    ref: "Category",
+    required: true
+  },
+  description: {
+    type: String
+  },
+  available: {
+    type: Boolean,
+    default: true
+  }
+})
+
+ProductSchema.methods.toJSON = function () {
+  const { __v, status, ...data } = this.toObject()
+  return data
+}
+
+export default model("Product", ProductSchema)
