52d78b9dedcbb3e47d7b9d0f5f8669f00eb2f78d
c4chris124 <c4chris124@gmail.com>
2023-05-09 15:48:23 -0600

    added product route to index routes

---
+++ routes/index.routes.js
@@ -0,0 +1,6 @@
+import userRoute from "./user.routes.js"
+import authRoute from "./auth.routes.js"
+import categoriesRoute from "./categories.routes.js"
+import productsRoute from "./products.routes.js"
+
+export { userRoute, authRoute, categoriesRoute, productsRoute }
