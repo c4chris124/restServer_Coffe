1c459e567e8b1272ec13053296102cbb49125c81
c4chris124 <c4chris124@gmail.com>
2023-05-09 15:32:30 -0600

    Importing new model on index and export for further use

--- models/index.js
+++ models/index.js
@@ -1,6 +1,7 @@
 import Category from "./category.js"
+import Product from "./product.js" 
 import Role from "./role.js"
 import Server from "./server.js"
 import User from "./user.js"
 
-export { Category, Role, Server, User }
+export { Category, Product, Role, Server, User }
