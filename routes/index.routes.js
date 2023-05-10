80d0206c306af84ea390ead2f1388198f60cae8d
c4chris124 <c4chris124@gmail.com>
2023-05-10 15:24:57 -0600

    Adding search route to index for imports

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -2,5 +2,6 @@ import userRoute from "./user.routes.js"
 import authRoute from "./auth.routes.js"
 import categoriesRoute from "./categories.routes.js"
 import productsRoute from "./products.routes.js"
+import searchRoute from "./search.routes.js"
 
-export { userRoute, authRoute, categoriesRoute, productsRoute }
+export { userRoute, authRoute, searchRoute, categoriesRoute, productsRoute }
