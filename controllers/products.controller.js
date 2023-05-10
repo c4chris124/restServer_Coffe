79f057789b69c325c3776d1733f4d9012714d46a
c4chris124 <c4chris124@gmail.com>
2023-05-09 18:48:48 -0600

    cahnges to get products: populate user and category, product by id: populate user name, create product changes: remove status and user, validation if product name already exist, genereated data added name uppercased, and save product, update product chanages: chekig if name added uppercase, changed name of updated product, created all deleted product

--- controllers/products.controller.js
+++ controllers/products.controller.js
@@ -9,9 +9,10 @@ const getProducts = async (req = request, res = response) => {
   const [total, products] = await Promise.all([
     Product.countDocuments(query),
     Product.find(query)
+      .populate("user", "name")
+      .populate("category", "name")
       .skip(Number(since))
       .limit(Number(limit))
-      .populate("category", "name")
   ])
   res.json({ total, products })
 }
@@ -19,19 +20,33 @@ const getProducts = async (req = request, res = response) => {
 // obtain product - populate - {}
 const getProductById = async (req = request, res = response) => {
   const { id } = req.params
-  const product = await Product.findById(id).populate("category", "name")
+  const product = await Product.findById(id)
+    .populate("user", "name")
+    .populate("category", "name")
   res.json(product)
 }
 
 // create product
 const createProduct = async (req = request, res = response) => {
-  const product = req.body
+  const { status, user, ...product } = req.body
+
+  const productOnDb = await Product.findOne({ name: product.name })
+
+  if (productOnDb) {
+    return res.status(400).json({
+      msg: `The Product with ${productOnDb.name}, already exist`
+    })
+  }
+
+  // generate data to save
   const data = {
     ...product,
+    name: product.name.toUpperCase(),
     user: req.user._id
   }
 
-  const newProduct = await new Product(data)
+  const newProduct = new Product(data)
+
   await newProduct.save()
   res.status(201).json(newProduct)
 }
@@ -40,12 +55,33 @@ const createProduct = async (req = request, res = response) => {
 const updateProduct = async (req = request, res = response) => {
   const { id } = req.params
   const { status, user, ...product } = req.body
-  data.user = req.user._id
 
-  const productUpdated = await Product.findByIdAndUpdate(id, product, {
+  if (product.name) {
+    product.name = product.name.toUpperCase()
+  }
+
+  product.user = req.user._id
+
+  const updatedProduct = await Product.findByIdAndUpdate(id, product, {
     new: true
   })
-  res.json(productUpdated)
+  res.json(updatedProduct)
 }
 
-export { getProducts, getProductById, createProduct, updateProduct }
+const deleteProduct = async (req = request, res = response) => {
+  const { id } = req.params
+  const product = await Product.findOneAndUpdate(
+    id,
+    { status: false },
+    { new: true }
+  )
+  res.json(product)
+}
+
+export {
+  getProducts,
+  getProductById,
+  createProduct,
+  updateProduct,
+  deleteProduct
+}
