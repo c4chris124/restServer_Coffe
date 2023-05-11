240cde181c6a28412c81a1af09923f8fe1f40bd1
c4chris124 <c4chris124@gmail.com>
2023-05-11 17:51:10 -0600

    Added new route to update files, validations with mongo id and collections passes a custom function with collection, collections allowed'

--- routes/uploads.routes.js
+++ routes/uploads.routes.js
@@ -1,11 +1,23 @@
 import { Router } from "express"
 import { check } from "express-validator"
-
-import { validateFields } from "../middlewares/validate-fields.js"
-import { loadFiles } from "../controllers/uploads.controller.js"
+import { validateFields } from "../middlewares/index.js"
+import { loadFiles, updateImgs } from "../controllers/uploads.controller.js"
+import { allowCollections } from "../helpers/index.js"
 
 const router = Router()
 
 router.post("/", loadFiles)
+
+router.put(
+  "/:collection/:id",
+  [
+    check("id", "Invalid mongo ID").isMongoId(),
+    check("collection").custom((c) =>
+      allowCollections(c, ["users", "products"])
+    ),
+    validateFields
+  ],
+  updateImgs
+)
 
 export default router
