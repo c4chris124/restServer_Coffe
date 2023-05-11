4ef853bc4af7cb8c1b35a6480588f19dd0ff31f4
c4chris124 <c4chris124@gmail.com>
2023-05-11 12:49:54 -0600

    Added uploads route to index file imports

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -3,5 +3,13 @@ import authRoute from "./auth.routes.js"
 import categoriesRoute from "./categories.routes.js"
 import productsRoute from "./products.routes.js"
 import searchRoute from "./search.routes.js"
+import uploadsRoute from "./uploads.routes.js"
 
-export { userRoute, authRoute, searchRoute, categoriesRoute, productsRoute }
+export {
+  userRoute,
+  authRoute,
+  searchRoute,
+  categoriesRoute,
+  productsRoute,
+  uploadsRoute
+}
