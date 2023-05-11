47701bf1dd1231d170c968966e0171ca296537dd
c4chris124 <c4chris124@gmail.com>
2023-05-11 13:27:36 -0600

    created all ffn to load files, added file url to path as i'm using es modules do not have global __dir variable, validations if file comes on req file, using file mv to move file to desire route, created uploads folder on root

--- controllers/uploads.controller.js
+++ controllers/uploads.controller.js
@@ -1,8 +1,29 @@
 import { response, request } from "express"
+import { fileURLToPath } from "url"
+import path from "path"
 
 const loadFiles = (req = request, res = response) => {
-  res.json({
-    mgs: "Load files route"
+  if (!req.files || Object.keys(req.files).length === 0 || !req.files.file) {
+    res.status(400).json({
+      msg: "No files on the request"
+    })
+    return
+  }
+
+  const { file } = req.files
+  const __filename = fileURLToPath(import.meta.url)
+  const __dirname = path.dirname(__filename)
+
+  const uploadPath = path.join(__dirname, "../uploads/", file.name)
+
+  file.mv(uploadPath, function (err) {
+    if (err) {
+      return res.status(500).json({ err })
+    }
+
+    res.json({
+      msg: `File uploaded to ${uploadPath}`
+    })
   })
 }
 
