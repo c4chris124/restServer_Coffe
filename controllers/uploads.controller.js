4e348eec7e0917531e4e1e0d683f7570e6c7e1f8
c4chris124 <c4chris124@gmail.com>
2023-05-11 15:40:54 -0600

    impored uuid for renaming files, validating files extentios, and renaming file with uuid

--- controllers/uploads.controller.js
+++ controllers/uploads.controller.js
@@ -1,6 +1,7 @@
 import { response, request } from "express"
 import { fileURLToPath } from "url"
 import path from "path"
+import { v4 as uuidv4 } from "uuid"
 
 const loadFiles = (req = request, res = response) => {
   if (!req.files || Object.keys(req.files).length === 0 || !req.files.file) {
@@ -11,10 +12,22 @@ const loadFiles = (req = request, res = response) => {
   }
 
   const { file } = req.files
+  const cutName = file.name.split(".")
+  const extension = cutName[cutName.length - 1]
+
+  // validate extension
+  const validExtensions = ["png", "JPG", "jpeg", "gif"]
+  if (!validExtensions.includes(extension)) {
+    return res.status(400).json({
+      msg: `The extension ${extension} is not allowed in ${validExtensions}`
+    })
+  }
+
+  const tempFileName = `${uuidv4()}.${extension}`
   const __filename = fileURLToPath(import.meta.url)
   const __dirname = path.dirname(__filename)
 
-  const uploadPath = path.join(__dirname, "../uploads/", file.name)
+  const uploadPath = path.join(__dirname, "../uploads/", tempFileName)
 
   file.mv(uploadPath, function (err) {
     if (err) {
